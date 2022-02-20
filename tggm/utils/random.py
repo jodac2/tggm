cdb2db7288c3e1d9d6c4b6f28319242ee55ae7d9
jodac2 <jdaniel.riveram@gmail.com>
2022-02-19 18:07:50 -0600

    Random-Normal

--- tggm/utils/random.py
+++ tggm/utils/random.py
@@ -1,123 +1,97 @@
-from cmath import exp
-from logging import raiseExceptions
-from unittest import expectedFailure
 import numpy as np
-import tensorly as tl
-from tggm.utils.utils import to_list
 
 
-def random_sigma(d, s=1, seed=None):
+def random_phi(d, randA=None, randD=None, rand="normal", params=None, params2=None, inv=False, seed=None):
     """
-    Random positive definite matrix(d,d)
+    Random Phi = Sigma^1
     ------
+        Random Phi = A'(D^-1)A
     
     Params
     ------
-    d: int to dimension of matrix
-    s: scale for variances
-    seed: seed to random
+    d: dimension of matrix
+    randA: Generator to upper diagonal elements of matrix A
+    randB: Genetator to diagonal elements of matrid D^-1
+    rand: distribution of elements of matrix A if randA is None
+        uniform and normal are possible
+    params: params of rand genetator to matrix A
+        -loc and scale to nomrmal
+        -low and high to uniform
+    params2: params of rand generator to matriz D
+        -scale and shape
+    inv: True is return Phi^-1
+    seed: seed
     
     Return
     ------
-    S: positive definite matrix(d,d)
+    Phi: random matrix d x d
     """
     
-    if s <= 0:
-        raise Exception("Value of s=%s shouldn't < 0" % s)
+    if d < 2:
+        raise Exception("Dimesion should be greater than 1")
     
-    if d < 1:
-        raise Exception("Value of d=%s shouldn't < 1 " % d)
+    if randA is None:
+        
+        if rand == "uniform":
+            
+            if params is None:
+                params = dict(low=-0.1, high=0.1)
+            else:
+                if "low" not in params.keys() or "high" not in params.keys():
+                    raise Exception("Param low or high not in params")
+                else:
+                    if params["low"] >= params["high"]:
+                        raise Exception("Param low should be smaller than high")
+            
+            def randA(size):
+                return np.random.uniform(size=size, **params)
+        
+        else:
+            
+            if params is None:
+                params = dict(loc=0, scale=0.1)
+            else:
+                if "loc" not in params.keys() or "scale" not in params.keys():
+                    raise Exception("Param loc or scale not in params")
+                else:
+                    if params["scale"] <= 0:
+                        raise Exception("Param loc should be greater than 0")
+            
+            def randA(size):
+                return np.random.normal(size=size, **params)
+    
+    if randD is None:
+        
+        if params2 is None:
+            params2 = dict(scale=1.0, shape=10)
+        else:
+            if "scale" not in params2.keys() or "shape" not in params2.keys():
+                raise Exception("Param scale or shape not in params2")
+            else:
+                if params2["scale"] <= 0 or params2["shape"] <= 0:
+                    raise Exception("Params scale and shape should be greater than 0")
+        
+        def randD(size):
+            return 1/np.random.gamma(size=size, **params2)
     
     np.random.seed(seed)
     
-    # Simetric matrix
-    A = np.random.uniform(size=d * d).reshape((d, d))
-    A = 0.5 * (A + A.T)
+    # Random matrix A
+    A = np.identity(d)
+    free_size = int(d * (d - 1)/2)
+    free_indices = np.triu_indices(d, 1)
+    A[free_indices] = randA(free_size)
     
-    # Enssured positive definite
-    A = A + d * np.identity(d)
+    # Random matrix D^-1
+    D = np.identity(d)
+    np.fill_diagonal(D, randD(d))
     
-    # Scale
-    sm = A.diagonal().max()
-    S = s * A/sm
+    # Random matrix Phi
+    Phi = A.T @ D @ A
     
-    np.random.seed(None)
-    
-    return S
-
-
-def random_tnorm(size=1, mu=None, sigma=None, shape=None, seed=None, stack=False):
-    """
-    Random tensor-normal
-    ------
-        Generated random tensor-normal(nu, sigma)
-    
-    Params
-    ------
-    size: size of sample
-    sigma: list with covariance matriz of shape (di, di), i=1, ..., n
-    mu: tensor mean of shape d1 x ... x dp 
-    shape: list with shape of random tensor, when mu and signa is None
-    seed: seed to simulation
-    stack: True is sample is stacked
-    
-    Return
-    ------
-    tnorm: list with tensors d1 x ... x dp if stack is False 
-        else tensor d1 x ... x dp x size
-    """
-    
-    if sigma is None and mu is None and shape is None:
-        raise Exception("Any of sigma, mu or shape should be not None")
-    
-    if size < 1:
-        raise Exception("Sample size should be > 0")
-    
-    if sigma is not None and mu is not None:
-        
-        shape = [si.shape[0] for si in sigma]
-        if shape != list(mu.shape):
-            raise Exception("Shape of sigma and mu should be equal")
-        
-    elif sigma is not None and mu is None:
-        
-        shape = [si.shape[0] for si in sigma]
-
-    elif sigma is None and mu is not None:
-        
-        shape = list(mu.shape)
-
-    else:
-
-        if type(shape) is not list:
-            raise Exception("Type of shape should be list")
-    
-    np.random.seed(seed)
-    
-    # Get A such S=AA'
-    if sigma is not None:
-        A = [np.linalg.cholesky(si) for si in sigma]
-    
-    # x ~ N(0, 1)
-    vnorm = np.random\
-        .normal(size=np.prod(shape) * size)\
-        .reshape((*shape, size))
-    
-    # t ~ TN(0, 1)
-    tnorm = tl.tensor(vnorm)
-    tnorm = [tnorm[..., i] for i in range(size)]
-    
-    # t ~ TN(0, sigma)
-    if sigma is not None:
-        tnorm = [tl.tenalg.multi_mode_dot(ti, A) for ti in tnorm]
-    
-    # t ~ TN(mu, sigma)
-    if mu is not None:
-        tnorm = [ti + mu for ti in tnorm]
-    
-    if stack:
-        tnorm = tl.stack(tnorm, axis=len(shape))
+    if inv:
+        Phi = np.linalg.inv(Phi)
     
     np.random.seed(None)
     
-    return tnorm
+    return Phi
